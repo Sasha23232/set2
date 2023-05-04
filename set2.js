014f0ef721524da5eee62ef0e2ca8ecc0a1ec747
Sasha23232 <103206823+Sasha23232@users.noreply.github.com>
2023-05-04 20:03:43 +0300

    Add files via upload

---
+++ set2.js
@@ -0,0 +1,13 @@
+let img = document.querySelector(".img");
+let h1 = document.querySelector(".h1");
+let ul = document.querySelector(".nav-ul");
+console.log(img, h1, ul);
+
+
+// openBtn.addEventListener("click", function () {
+//   container.classList.add("show");
+// });
+
+// closeBtn.addEventListener("click", function () {
+//   container.classList.remove("show");
+// });
